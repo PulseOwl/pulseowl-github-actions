baa187d81d7e76ae8fe95ec0875a55b68320b016
Jorge A. Borges <jorge.borges@hamiware.com>
2026-02-14 19:32:45 +1100

    feat: add owner data to payload

--- actions/collector/src/index.ts
+++ actions/collector/src/index.ts
@@ -118,6 +118,8 @@ async function run(): Promise<void> {
       github: {
         repository: process.env.GITHUB_REPOSITORY,
         repository_id: process.env.GITHUB_REPOSITORY_ID,
+        repository_owner: process.env.GITHUB_REPOSITORY_OWNER,
+        repository_owner_id: process.env.GITHUB_REPOSITORY_OWNER_ID,
         run_id: process.env.GITHUB_RUN_ID,
         run_attempt: process.env.GITHUB_RUN_ATTEMPT,
         workflow: process.env.GITHUB_WORKFLOW,
